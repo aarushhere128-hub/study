6888a5cb15b131e3775af2d93998b14087ecdab5
aarushhere128-hub <aarushhere128@gmail.com>
2026-08-27 19:43:29 +0530

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "study-engine-v1";
+const CACHE_NAME = "study-engine-v2";
 
 const FILES_TO_CACHE = [
     "./",
@@ -8,11 +8,14 @@ const FILES_TO_CACHE = [
     "./manifest.json"
 ];
 
+
 self.addEventListener("install", event => {
 
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(cache =>
+                cache.addAll(FILES_TO_CACHE)
+            )
     );
 
     self.skipWaiting();
@@ -22,19 +25,65 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
 
     event.waitUntil(
+
         caches.keys().then(keys =>
+
             Promise.all(
+
                 keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
+                    .filter(
+                        key =>
+                            key !== CACHE_NAME
+                    )
+                    .map(
+                        key =>
+                            caches.delete(key)
+                    )
+
             )
+
         )
+
     );
 
     self.clients.claim();
 });
 
 
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        fetch(event.request)
+
+            .then(response => {
+
+                const responseClone =
+                    response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+
+                        cache.put(
+                            event.request,
+                            responseClone
+                        );
+
+                    });
+
+                return response;
+
+            })
+
+            .catch(() =>
+                caches.match(
+                    event.request
+                )
+            )
+
+    );
+
+});
 self.addEventListener("fetch", event => {
 
     event.respondWith(
